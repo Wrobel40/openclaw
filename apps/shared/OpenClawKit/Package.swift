14a52940ccfd793763891b2ebb2b1274fbb6a55d
Wrobel40 <mpaszkowski85@gmail.com>
2026-02-14 09:23:13 +0100

    Use Xcode 15.4 with iOS 16

--- apps/shared/OpenClawKit/Package.swift
+++ apps/shared/OpenClawKit/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "OpenClawKit",
     platforms: [
-        .iOS(.v18),
+        .iOS(.v16),
         .macOS(.v13),
     ],
     products: [
