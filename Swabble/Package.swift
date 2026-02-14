b3997c4f364682894244eb1ffe46a577bd94b5df
Wrobel40 <mpaszkowski85@gmail.com>
2026-02-14 08:48:01 +0100

    Remove Swift 6 requirement completely

--- Swabble/Package.swift
+++ Swabble/Package.swift
@@ -4,53 +4,52 @@ import PackageDescription
 let package = Package(
     name: "swabble",
     platforms: [
-        .macOS(.v13),
-        .iOS(.v16),
+        .macOS(.v13)
+        .iOS(.v16)
     ],
     products: [
-        .library(name: "Swabble", targets: ["Swabble"]),
-        .library(name: "SwabbleKit", targets: ["SwabbleKit"]),
-        .executable(name: "swabble", targets: ["SwabbleCLI"]),
+        .library(name: "Swabble", targets: ["Swabble"])
+        .library(name: "SwabbleKit", targets: ["SwabbleKit"])
+        .executable(name: "swabble", targets: ["SwabbleCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/steipete/Commander.git", exact: "0.2.1"),
-        .package(url: "https://github.com/apple/swift-testing", from: "0.99.0"),
+        .package(url: "https://github.com/steipete/Commander.git", exact: "0.2.1")
+        .package(url: "https://github.com/apple/swift-testing", from: "0.99.0")
     ],
     targets: [
         .target(
             name: "Swabble",
             path: "Sources/SwabbleCore",
-            swiftSettings: []),
+            swiftSettings: [])
         .target(
             name: "SwabbleKit",
             path: "Sources/SwabbleKit",
             swiftSettings: [
-                .enableUpcomingFeature("StrictConcurrency"),
-            ]),
+                .enableUpcomingFeature("StrictConcurrency")
+            ])
         .executableTarget(
             name: "SwabbleCLI",
             dependencies: [
                 "Swabble",
                 "SwabbleKit",
-                .product(name: "Commander", package: "Commander"),
+                .product(name: "Commander", package: "Commander")
             ],
-            path: "Sources/swabble"),
+            path: "Sources/swabble")
         .testTarget(
             name: "SwabbleKitTests",
             dependencies: [
                 "SwabbleKit",
-                .product(name: "Testing", package: "swift-testing"),
+                .product(name: "Testing", package: "swift-testing")
             ],
             swiftSettings: [
-                .enableUpcomingFeature("StrictConcurrency"),
-                .enableExperimentalFeature("SwiftTesting"),
-            ]),
+                .enableUpcomingFeature("SrtictConcurrency")
+                .enableExperimentalFeature("SwiftTesting")
+            ])
         .testTarget(
             name: "swabbleTests",
             dependencies: [
                 "Swabble",
-                .product(name: "Testing", package: "swift-testing"),
-            ]),
+                .product(name: "Testing", package: "swift-testing")
+            ])
     ],
-    // swiftLanguageModes: [.v6] // disabled for CI
-
+)
